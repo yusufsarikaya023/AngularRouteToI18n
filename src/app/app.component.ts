31f7e9f31ce6ba37aa039ee00442f9ec85bbf047
yusufsarikaya023 <yusufsarikaya023@gmail.com>
2024-04-07 01:43:02 +0300

    prepare for route and content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { RouteConsts } from './app.routes';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'AngularRouteToI18n';
+  routes = [
+    { link: RouteConsts.Home, name: $localize`Home` },
+    {
+      link: RouteConsts.FrequentlyAskedQuestion,
+      name: $localize`Frequently Asked Questions`,
+    },
+    { link: RouteConsts.Contact, name: $localize`Contact` },
+  ];
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { QuestionsComponent } from './questions/questions.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export class RouteConsts {
+  public static readonly Home = $localize`:@@home:home`;
+  public static readonly FrequentlyAskedQuestion = $localize`:@@frequently-asked-questions:frequently-asked-questions`;
+  public static readonly Contact = $localize`:@@contact:contact`;
+}
+
+export const routes: Routes = [
+  { path: '', redirectTo: RouteConsts.Home, pathMatch: 'full' },
+  { path: RouteConsts.Home, component: HomeComponent },
+  { path: RouteConsts.FrequentlyAskedQuestion, component: QuestionsComponent },
+  { path: RouteConsts.Contact, component: ContactComponent },
+];
